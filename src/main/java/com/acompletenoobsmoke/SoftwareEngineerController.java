35bfdc24d8d2623323f33c04ec7b1bf7df2063cc
OmOmofonmwan <omomofonmwan@tinkarbuild.com>
2025-09-21 12:39:42 -0500

    Added Docker compose file

--- src/main/java/com/acompletenoobsmoke/SoftwareEngineerController.java
+++ src/main/java/com/acompletenoobsmoke/SoftwareEngineerController.java
@@ -1,4 +1,26 @@
 package com.acompletenoobsmoke;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer>  getEngineers() {
+        return engineerList();
+    }
+
+    private List<SoftwareEngineer> engineerList() {
+        return List.of(new SoftwareEngineer(1, "Osaretin", "Omofonmwan", "Java"),
+                       new SoftwareEngineer(2, "Victoria", "Marshall", "Javascript"),
+                       new SoftwareEngineer(3, "Adedayo", "Uwensuyi", "Python"),
+                       new SoftwareEngineer(4, "Ajinboye", "Uwensuyi", "ServiceNow/Javascript"),
+                       new SoftwareEngineer(5, "Dustin", "Carpenter", "C++")
+        );
+    }
 }
